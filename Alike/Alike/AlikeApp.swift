2885f74571b3de6877ee29b673692f6b41499033
Oleksandr Solokha <s.s@dcjudgement.com>
2026-01-27 17:39:07 +0200

    ✨ Init: add initial Xcode project structure

---
+++ Alike/Alike/AlikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlikeApp.swift
+//  Alike
+//
+//  Created by Oleksand S on 27.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct AlikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alike/Alike/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alike
+//
+//  Created by Oleksand S on 27.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
